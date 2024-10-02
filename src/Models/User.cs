2c16e765f4d9492354461661f9ef0df718c1702d
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2024-10-02 20:39:51 -0300

    first commit

---
+++ src/Models/User.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using Bogus.DataSets;
+
+namespace Catedra1_Gabriel_Cruz.src.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Rut { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Correo { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression("^(masculino|femenino|otro|prefiero no decirlo)$", ErrorMessage = "Las categorías permitidas son: 'masculino', 'femenino', 'otro' o 'prefiero no decirlo'.")]
+        public string Genero { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
